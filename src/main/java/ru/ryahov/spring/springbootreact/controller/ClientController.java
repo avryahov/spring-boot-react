65ae7dd904e3b3991a6f80623d3d7333d1041547
ryahov_av <a.v.ryahov@gmail.com>
2021-06-30 14:27:18 +0500

    Add Client controller

---
+++ src/main/java/ru/ryahov/spring/springbootreact/controller/ClientController.java
@@ -0,0 +1,52 @@
+package ru.ryahov.spring.springbootreact.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.ryahov.spring.springbootreact.model.Client;
+import ru.ryahov.spring.springbootreact.repository.ClientRepository;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/clients")
+public class ClientController {
+
+    private final ClientRepository clientRepository;
+
+    public ClientController(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public List<Client> getClients() {
+        return clientRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Client getClient(@PathVariable Long id) {
+        return clientRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    @PostMapping
+    public ResponseEntity createClient(@RequestBody Client client) throws URISyntaxException {
+        Client savedClient = clientRepository.save(client);
+        return ResponseEntity.created(new URI("/clients/" + savedClient.getId())).body(savedClient);
+    }
+
+    @PostMapping
+    public ResponseEntity updateClient(@PathVariable Long id, @RequestBody Client client) {
+        Client currentClient = clientRepository.findById(id).orElseThrow(RuntimeException::new);
+        currentClient.setName(client.getName());
+        currentClient.setEmail(client.getEmail());
+        currentClient = clientRepository.save(client);
+
+        return ResponseEntity.ok(currentClient);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteClient(@PathVariable Long id) {
+        clientRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+}
