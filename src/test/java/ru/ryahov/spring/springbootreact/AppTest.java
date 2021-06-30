bc941633351d6b065db5aa3e261e167042732085
ryahov_av <a.v.ryahov@gmail.com>
2021-06-30 12:03:39 +0500

    Init project

---
+++ src/test/java/ru/ryahov/spring/springbootreact/AppTest.java
@@ -0,0 +1,18 @@
+package ru.ryahov.spring.springbootreact;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
